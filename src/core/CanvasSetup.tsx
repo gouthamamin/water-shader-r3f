ec73e6151ad1c66d77d1e516464ed947a6c2edd8
Goutham Amin <gouthamamin@gmail.com>
2025-11-30 18:18:46 +0530

    update : add camera position

--- src/core/CanvasSetup.tsx
+++ src/core/CanvasSetup.tsx
@@ -4,7 +4,12 @@ import type React from "react";
 const CanvasSetup = ({ children }: React.PropsWithChildren) => {
 
   return (
-    <Canvas>
+    <Canvas camera={{
+      position: [0, 10, 20],
+      fov: 60,
+      near: 0.1,
+      far: 1000
+    }}>
       {children}
     </Canvas>
   )
